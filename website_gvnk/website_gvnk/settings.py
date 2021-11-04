e31efbac3a155bae92f163f55d9b49a8d94ac1b1
NiKiing <nikogv04@gmail.com>
2021-11-03 19:36:38 -0500

    The Django setting was configured

--- website_gvnk/website_gvnk/settings.py
+++ website_gvnk/website_gvnk/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'website',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'website_gvnk.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'homeworks_data',
+        'USER': 'nikiing',
+        'PASSWORD':'NIKIINGPORTAFOLIO',
+        'HOST':'localhost',
+        'PORT': '5432',
     }
 }
 
