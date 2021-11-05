b5cd0d28a4bbd8cb1c120d2c3385eaaba80f6086
NiKiing <nikogv04@gmail.com>
2021-11-05 00:24:30 -0500

    The main section was created with the file JS(HTML/CSS/JS)

---
+++ website_gvnk/static/js/scripts.js
@@ -0,0 +1,12 @@
+//INICIALIZED AOS LIBRERY
+AOS.init();
+
+let menu = document.getElementById("menu");
+
+let menu_bar = document.getElementById("menu-bar");
+
+menu_bar.addEventListener('click', function(){
+    
+    menu.classList.toggle('menu-toggle')
+
+})
