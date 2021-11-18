2d528415097971a2caca0cb7f0473c9989ffac76
NiKiing <nikogv04@gmail.com>
2021-11-18 16:54:07 -0500

    Commit to added the staticfiles in the settings.py

--- website_gvnk/website_gvnk/settings.py
+++ website_gvnk/website_gvnk/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
+from pathlib import Path, os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,7 +124,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/home/nikiing/Desktop/website-gvnk/website_gvnk/static',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+9
